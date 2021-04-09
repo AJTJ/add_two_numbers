3c02f579921eba93bfcf61b4c3ca817b2d638648
AJTJ <janke.aaron@gmail.com>
2021-04-09 15:00:08 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -47,8 +47,9 @@ fn main() {
     let list_a1 = ListNode::new(1);
 
     let list_a2 = push_front(list_a1, 2);
+    let list_a3 = push_front(list_a2, 3);
 
-    let list_a = push_front(list_a2, 3);
+    let list_a = push_front(list_a3, 4);
 
     let list_b1 = ListNode::new(4);
 
@@ -56,16 +57,15 @@ fn main() {
 
     let list_b = push_front(list_b2, 6);
 
-    println!("{:?}", list_a);
-    println!("{:?}", list_b);
-    println!("{:?}", list_b);
+    println!("a: `{:?}", list_a);
+    println!("b: {:?}", list_b);
 
     fn create_vec(list: Option<Box<ListNode>>) -> Vec<i32> {
-        let mut num_vec: Vec<i32>;
+        let mut num_vec = Vec::new();
         fn unravel(list: Option<Box<ListNode>>, num_vec: &mut Vec<i32>) -> Vec<i32> {
             if let Some(list) = list {
                 num_vec.push(list.val);
-                if let Some(list) = list.next {
+                if let Some(_) = list.next {
                     unravel(list.next, num_vec)
                 } else {
                     num_vec.clone()
@@ -77,6 +77,9 @@ fn main() {
         unravel(list, &mut num_vec);
         num_vec
     }
+
+    println!("a: {:?}", create_vec(Some(Box::new(list_a))));
+    println!("b: {:?}", create_vec(Some(Box::new(list_b))));
 
     // fn add_two_numbers(
     //     l1: Option<Box<ListNode>>,
